a308a2a3cac7771034042abfa94cfdc2d73aa10d
Karthikeyan's Mac <anumallakarthikeyan03@gmail.com>
2025-12-06 20:54:41 +0530

    configured setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CheckMyVisa",
+    version="0.0.0",
+    author="Karthikeyan Anumalla",
+    author_email="dummy-email@gmail.com",
+    packages=find_packages()
+)
